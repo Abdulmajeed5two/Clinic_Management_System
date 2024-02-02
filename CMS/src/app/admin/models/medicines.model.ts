8456c7a209f005a7bad49fa4babab7e1a2021b4c
Abdul Majeed <154789909+Abdulmajeed52@users.noreply.github.com>
2024-02-02 10:41:54 +0500

    done

---
+++ CMS/src/app/admin/models/medicines.model.ts
@@ -0,0 +1,7 @@
+export class medicines {
+    id : string;
+    medicine_Name : string;
+    medicine_Price : string;
+    medicine_Type : string;
+    medicine_Description : string
+}
---
+++ CMS/src/app/user/profile/profile.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserStoreService } from 'src/app/services/user-store.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+
+  public users:any = [];
+  public role!:string;
+
+  public fullName : string = "";
+  constructor(private api : ApiService, private auth: AuthService, private userStore: UserStoreService) { }
+
+  ngOnInit() {
+    this.api.getUsers()
+    .subscribe(res=>{
+    this.users = res;
+    });
+
+    this.userStore.getFullNameFromStore()
+    .subscribe(val=>{
+      const fullNameFromToken = this.auth.getfullNameFromToken();
+      this.fullName = val || fullNameFromToken
+    });
+
+    this.userStore.getRoleFromStore()
+    .subscribe(val=>{
+      const roleFromToken = this.auth.getRoleFromToken();
+      this.role = val || roleFromToken;
+    })
+  }
+
+  logout(){
+    this.auth.signOut();
+  }
+}
